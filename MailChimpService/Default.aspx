1e7eeafb3331d78b9ed8f8c86bef4179a4112c54
rvydhya <rvydhya@outlook.com>
2015-09-19 13:25:54 +0530

    first commit

---
+++ MailChimpService/Default.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="Home Page" Language="C#" Async="true" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MailChimpService._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    
+    <asp:ImageButton ID="Button1" runat="server" ImageUrl="~/mailchimp.jpg" OnClick="Button1_Click" />
+
+</asp:Content>
---
+++ MailChimpService/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿
+using Mandrill;
+using Mandrill.Models;
+using Mandrill.Requests.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MailChimpService
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        protected async void Button1_Click(object sender, EventArgs e)
+        {
+
+
+
+
+                MandrillApi api = new MandrillApi("Mandrill Api Key", true);
+
+                IEnumerable<EmailAddress> addresses = new[]
+            {
+                new EmailAddress("Toemailid@xxx.xx", "Name Of the Contact"),
+                
+            };
+
+
+
+
+                var message = new EmailMessage();
+
+                message.FromEmail = "Frommail@xxx.xxx";
+                message.FromName = "Your name";
+            message.Html = "emailbody";
+                message.Text = "emailbodytext";
+            message.Subject = "subject";
+                message.To = addresses;
+              
+
+                SendMessageRequest req = new SendMessageRequest(message);
+
+                var returnvalue = await api.SendMessage(req);
+             
+            }
+
+
+     
+
+      
+    }
+}
